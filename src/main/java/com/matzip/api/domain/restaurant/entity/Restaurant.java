c1220a68d7b1e689058bd94806db6ea9e59c1790
bellringstar <bellringstar@gmail.com>
2024-07-09 22:35:38 +0900

    refact: 네이버의 데이터를 저장할 수 없으니 가게를 식별하기 위한 최소한의 정보만 저장하도록 변경

--- src/main/java/com/matzip/api/domain/restaurant/entity/Restaurant.java
+++ src/main/java/com/matzip/api/domain/restaurant/entity/Restaurant.java
@@ -4,10 +4,7 @@ import com.matzip.api.common.entity.BaseTimeEntity;
 import com.matzip.api.domain.review.entity.Review;
 import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
-import jakarta.persistence.Embedded;
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
@@ -25,20 +22,11 @@ import lombok.NoArgsConstructor;
 public class Restaurant extends BaseTimeEntity {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    @Column(name = "external_id")
+    private String externalId;
 
     @Column(nullable = false)
     private String name;
-
-    @Embedded
-    private Location location;
-
-    @Column(nullable = false)
-    private String category;
-
-    @Column(nullable = false)
-    private Double averageRating = 0.0;
 
     @OneToMany(mappedBy = "restaurant", cascade = CascadeType.ALL, orphanRemoval = true)
     private List<RestaurantCharacteristic> characteristics = new ArrayList<>();
@@ -46,44 +34,9 @@ public class Restaurant extends BaseTimeEntity {
     @OneToMany(mappedBy = "restaurant")
     private List<Review> reviews = new ArrayList<>();
 
-    @Column(nullable = false)
-    private String externalId;
-
     @Builder
-    public Restaurant(String name, Location location, String category, Double averageRating, String externalId) {
-        this.name = name;
-        this.location = location;
-        this.category = category;
-        this.averageRating = averageRating;
+    public Restaurant(String externalId, String name) {
         this.externalId = externalId;
-    }
-
-    public static Restaurant createRestaurant(String name, Location location, String category, Double averageRating,
-                                              String externalId) {
-        Restaurant restaurant = new Restaurant();
-        restaurant.name = name;
-        restaurant.location = location;
-        restaurant.category = category;
-        restaurant.averageRating = 0.0;
-        restaurant.externalId = externalId;
-
-        return restaurant;
-    }
-
-    public void updateAverageRating() {
-        this.averageRating = reviews.stream()
-                .mapToDouble(Review::getRating)
-                .average()
-                .orElse(0.0);
-    }
-
-    public void addReview(Review review) {
-        this.reviews.add(review);
-        updateAverageRating();
-    }
-
-    public void removeReview(Review review) {
-        this.reviews.remove(review);
-        updateAverageRating();
+        this.name = name;
     }
 }
