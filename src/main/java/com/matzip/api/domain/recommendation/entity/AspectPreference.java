9e32e0492051dce436bd8c62bab23424f10b8762
bellringstar <bellringstar@gmail.com>
2024-07-10 23:07:44 +0900

    feat: AspectPreference 선호도 점수 계산을 위한 가중치 필드 추가 및 조정 메서드 추가

--- src/main/java/com/matzip/api/domain/recommendation/entity/AspectPreference.java
+++ src/main/java/com/matzip/api/domain/recommendation/entity/AspectPreference.java
@@ -37,13 +37,30 @@ public class AspectPreference {
     @Column(nullable = false)
     private Double score;
 
+    @Column(nullable = false)
+    private Double weight;
+
+    public static final Double DEFAULT_WEIGHT = 1.0;
+
     public AspectPreference(UserPreference userPreference, RestaurantAspect aspect, Double score) {
         this.userPreference = userPreference;
         this.aspect = aspect;
         this.score = score;
+        this.weight = DEFAULT_WEIGHT;
     }
 
     public void updateScore(Double score) {
         this.score = score;
+    }
+
+    public void adjustWeight(Double adjustment) {
+        // 사용자의 특정 행동(예를 들어 특정 부분에 자주 높은 점수를 준다) 이벤트 발생시 실행
+        this.weight += adjustment;
+        if (this.weight < 0) this.weight = 0.0;
+        if (this.weight > 2) this.weight = 2.0;
+    }
+
+    public Double getWeightedScore() {
+        return score * weight;
     }
 }
