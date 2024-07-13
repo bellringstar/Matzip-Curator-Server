5392dcef539b0b3a242dab258227871eb4dfe500
bellringstar <bellringstar@gmail.com>
2024-07-13 23:15:18 +0900

    feat: UserPreferenceServiceImpl, UserPreferenceService
    사용자의 선호도를 업데이트 하는 메서드 작성 및 사용자 생성 이벤트를 받아 선호도를 설정하는 이벤트리스너 추가

--- src/main/java/com/matzip/api/domain/recommendation/service/UserPreferenceService.java
+++ src/main/java/com/matzip/api/domain/recommendation/service/UserPreferenceService.java
@@ -1,8 +1,13 @@
 package com.matzip.api.domain.recommendation.service;
 
 import com.matzip.api.domain.recommendation.entity.UserPreference;
+import com.matzip.api.domain.recommendation.enums.RestaurantAspect;
+import com.matzip.api.domain.user.event.UserCreatedEvent;
+import java.util.Map;
 
 public interface UserPreferenceService {
 
+    void handleUserCreated(UserCreatedEvent event);
+    void updateUserPreference(Long userId, Map<RestaurantAspect, Double> newScores);
     UserPreference getUserPreference(String userId);
 }
--- src/main/java/com/matzip/api/domain/recommendation/service/UserPreferenceServiceImpl.java
+++ src/main/java/com/matzip/api/domain/recommendation/service/UserPreferenceServiceImpl.java
@@ -1,8 +1,18 @@
 package com.matzip.api.domain.recommendation.service;
 
+import com.matzip.api.common.error.UserPreferenceErrorCode;
+import com.matzip.api.common.event.DomainEventPublisher;
+import com.matzip.api.common.exception.ApiException;
+import com.matzip.api.domain.recommendation.entity.AspectPreference;
 import com.matzip.api.domain.recommendation.entity.UserPreference;
+import com.matzip.api.domain.recommendation.enums.RestaurantAspect;
+import com.matzip.api.domain.recommendation.event.UserPreferenceUpdatedEvent;
+import com.matzip.api.domain.recommendation.repository.UserPreferenceRepository;
+import com.matzip.api.domain.user.event.UserCreatedEvent;
+import java.util.Map;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +20,40 @@ import org.springframework.stereotype.Service;
 @Slf4j
 public class UserPreferenceServiceImpl implements
         UserPreferenceService {
+
+    private final UserPreferenceRepository userPreferenceRepository;
+    private final DomainEventPublisher eventPublisher;
+
+    @EventListener
+    @Override
+    public void handleUserCreated(UserCreatedEvent event) {
+        UserPreference preference = UserPreference.createForUser(event.getUser());
+        userPreferenceRepository.save(preference);
+    }
+
+    @Override
+    public void updateUserPreference(Long userId, Map<RestaurantAspect, Double> newScores) {
+        UserPreference userPreference = userPreferenceRepository.findById(userId)
+                .orElseThrow(() -> new ApiException(UserPreferenceErrorCode.USER_PREFERENCE_NOT_FOUND));
+
+        newScores.forEach(((aspect, score) -> {
+            userPreference.updateAspectPreference(aspect, score);
+            AspectPreference updatedAspect = userPreference.getAspectPreferences().stream()
+                    .filter(ap -> ap.getAspect() == aspect)
+                    .findFirst()
+                    .orElseThrow();
+
+        }));
+
+        userPreferenceRepository.save(userPreference);
+
+        eventPublisher.publish(new UserPreferenceUpdatedEvent(
+                userPreference.getId(),
+                userPreference.getUser().getUsername(),
+                userPreference
+        ));
+    }
+
     @Override
     public UserPreference getUserPreference(String userId) {
         return null;
