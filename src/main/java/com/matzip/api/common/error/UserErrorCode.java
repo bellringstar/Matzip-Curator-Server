66234580a76467cef8ee74db627c8bbe4a4faa04
bellringstar <bellringstar@gmail.com>
2024-07-14 20:49:11 +0900

    feat: UserErrorCode
    사용자 로그인id 및 이메일 중복 예외 추가

--- src/main/java/com/matzip/api/common/error/UserErrorCode.java
+++ src/main/java/com/matzip/api/common/error/UserErrorCode.java
@@ -8,7 +8,9 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum UserErrorCode implements ErrorCodeIfs {
 
-    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 5000, "유저가 존재하지 않습니다.");
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND.value(), 5000, "유저가 존재하지 않습니다."),
+    DUPLICATE_LOGIN_ID(HttpStatus.NOT_FOUND.value(), 5001, "이미 존재하는 id입니다."),
+    DUPLICATE_EMAIL(HttpStatus.NOT_FOUND.value(), 5002, "이미 존재하는 email입니다.");
 
     private final Integer httpStatusCode;
     private final Integer errorCode;
