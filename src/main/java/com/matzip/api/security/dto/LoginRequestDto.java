27a81ad0648704fad40e98f3b355392079a01cab
bellringstar <bellringstar@gmail.com>
2024-07-14 21:28:44 +0900

    feat: LoginRequestDto
    id, password 기반 일반회원 로그인 dto

---
+++ src/main/java/com/matzip/api/security/dto/LoginRequestDto.java
@@ -0,0 +1,21 @@
+package com.matzip.api.security.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lombok.ToString;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+@EqualsAndHashCode
+@ToString
+public class LoginRequestDto {
+
+    private String loginId;
+
+    private String password;
+}
