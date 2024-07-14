cf3965ce07013e9f6a8de024c191782627d1d6a7
bellringstar <bellringstar@gmail.com>
2024-07-14 20:13:22 +0900

    feat: UserRepository
    사용자의 loginId와 email로 해당 사용자를 찾아오는 메서드 추가

--- src/main/java/com/matzip/api/domain/user/repository/UserRepository.java
+++ src/main/java/com/matzip/api/domain/user/repository/UserRepository.java
@@ -1,7 +1,12 @@
 package com.matzip.api.domain.user.repository;
 
 import com.matzip.api.domain.user.entity.User;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
+    Optional<User> findByLoginId(String loginId);
+
+    Optional<User> findByEmail(String email);
 }
