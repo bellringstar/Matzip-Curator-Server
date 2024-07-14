d15d6b784a82b424474b70caac15c296a5e72dc3
bellringstar <bellringstar@gmail.com>
2024-07-14 20:12:43 +0900

    refact: UserService
    관련 로직 제거. 해당 로직은 authService로 이동

--- src/main/java/com/matzip/api/domain/user/service/UserService.java
+++ src/main/java/com/matzip/api/domain/user/service/UserService.java
@@ -1,12 +1,10 @@
 package com.matzip.api.domain.user.service;
 
 import com.matzip.api.common.event.DomainEventPublisher;
-import com.matzip.api.domain.user.dto.UserCreateRequestDto;
-import com.matzip.api.domain.user.entity.User;
-import com.matzip.api.domain.user.event.UserCreatedEvent;
 import com.matzip.api.domain.user.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -18,12 +16,5 @@ public class UserService {
 
     private final UserRepository userRepository;
     private final DomainEventPublisher eventPublisher;
-
-    public void createUser(UserCreateRequestDto requestDto) {
-
-        User user = User.createUser(requestDto);
-        user = userRepository.save(user);
-        // 이벤트 발생
-        eventPublisher.publish(new UserCreatedEvent(user));
-    }
+    private final PasswordEncoder passwordEncoder;
 }
