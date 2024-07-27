e90eed5c851c960de9fac232f8a228adaf833624
bellringstar <bellringstar@gmail.com>
2024-07-27 17:26:33 +0900

    feat: QueryDSl 사용을 위한 팩토리 빈 등록

--- src/main/java/com/matzip/api/config/AppConfig.java
+++ src/main/java/com/matzip/api/config/AppConfig.java
@@ -1,5 +1,8 @@
 package com.matzip.api.config;
 
+import com.querydsl.jpa.impl.JPAQueryFactory;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
@@ -10,5 +13,13 @@ public class AppConfig {
     @Bean
     public RestTemplate restTemplate() {
         return new RestTemplate();
+    }
+
+    @PersistenceContext
+    private EntityManager entityManager;
+
+    @Bean
+    public JPAQueryFactory jpaQueryFactory() {
+        return new JPAQueryFactory(entityManager);
     }
 }
