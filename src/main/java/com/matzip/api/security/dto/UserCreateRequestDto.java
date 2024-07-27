ee164e52a03f21e5bcfb0eb1aec467c36d1ebeac
bellringstar <bellringstar@gmail.com>
2024-07-27 12:48:51 +0900

    refact: vo 도입으로 인한 코드 변경

--- src/main/java/com/matzip/api/security/dto/UserCreateRequestDto.java
+++ src/main/java/com/matzip/api/security/dto/UserCreateRequestDto.java
@@ -1,7 +1,9 @@
 package com.matzip.api.security.dto;
 
 import com.matzip.api.domain.user.enums.AuthProvider;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,18 +12,23 @@ import lombok.ToString;
 
 @NoArgsConstructor
 @AllArgsConstructor
+@Builder
 @Getter
 @Setter
 @EqualsAndHashCode
 @ToString
 public class UserCreateRequestDto {
 
+    @NotNull
     private String loginId;
 
+    @NotNull
     private String email;
 
+    @NotNull
     private String password;
 
+    @NotNull
     private String name;
 
     private Boolean emailVerified = false;
