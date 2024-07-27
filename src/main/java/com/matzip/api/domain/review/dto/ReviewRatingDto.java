20dbd9898707d5d2209fd40faa423fac84cbdcfe
bellringstar <bellringstar@gmail.com>
2024-07-27 12:25:11 +0900

    feat: ReviewRatingDto
    리뷰 레이팅을 위한 dto
    기존에는 reviewId가 not null 이었는데 새로 생성하는 리뷰에 바로 레이팅이 함께 추가되는 경우를 고려해 null로 변경하고 추가시 설정해주도록 처리.
    이를 위해 별도 주석 설명 추가

--- src/main/java/com/matzip/api/domain/review/dto/ReviewRatingDto.java
+++ src/main/java/com/matzip/api/domain/review/dto/ReviewRatingDto.java
@@ -1,6 +1,8 @@
 package com.matzip.api.domain.review.dto;
 
 import com.matzip.api.domain.recommendation.enums.RestaurantAspect;
+import com.matzip.api.domain.review.entity.Review;
+import com.matzip.api.domain.review.entity.ReviewRating;
 import jakarta.validation.constraints.Max;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotNull;
@@ -17,9 +19,9 @@ import lombok.ToString;
 @Getter
 @Setter
 @ToString
-public class ReviewRatingRequestDto {
+public class ReviewRatingDto {
 
-    @NotNull
+    // null인 경우 새로 생성되는 리뷰에 대한 레이팅 null이 아닌 경우 기존의 리뷰에 추가하는 점수
     private Long reviewId;
 
     @NotNull
@@ -29,4 +31,12 @@ public class ReviewRatingRequestDto {
     @Min(1)
     @Max(5)
     private double rating;
+
+    public static ReviewRatingDto toDto(ReviewRating rating) {
+        return ReviewRatingDto.builder()
+                .reviewId(rating.getReview().getId())
+                .aspect(rating.getAspect())
+                .rating(rating.getRating().getScore())
+                .build();
+    }
 }
