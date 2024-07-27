2829e659562f30c50f41f7667323f5cc42687b94
bellringstar <bellringstar@gmail.com>
2024-07-27 11:21:28 +0900

    refact: ReviewErrorCode 변경
    전체 점수와 개별 점수 조건이 똑같기 떄문에 통일된 에러로 처리

--- src/main/java/com/matzip/api/common/error/ReviewErrorCode.java
+++ src/main/java/com/matzip/api/common/error/ReviewErrorCode.java
@@ -8,7 +8,7 @@ import org.springframework.http.HttpStatus;
 @Getter
 public enum ReviewErrorCode implements ErrorCodeIfs {
 
-    INVALID_OVERALL_RATING(HttpStatus.BAD_REQUEST.value(), 9000, "overall rating은 1~5 사이의 값만 가능합니다."),
+    INVALID_RATING(HttpStatus.BAD_REQUEST.value(), 9000, "overall rating은 1~5 사이의 값만 가능합니다."),
     INVALID_CONTENT(HttpStatus.BAD_REQUEST.value(), 9001, "content의 길이는 10~1000 사이만 가능합니다."),
     NOT_FOUND(HttpStatus.BAD_REQUEST.value(), 9002, "존재하지 않는 리뷰입니다.");
 
