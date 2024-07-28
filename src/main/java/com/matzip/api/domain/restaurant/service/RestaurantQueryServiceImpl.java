0813fba66886e50fdd73f26409485ce0c67ff8d7
bellringstar <bellringstar@gmail.com>
2024-07-28 14:13:41 +0900

    feat RestaurantQueryServiceImpl
    외부서비에 요청해 가게 목록을 가져와 내부 기준에 맞게 필터링 및 정렬해 반환한느 메서드 구현

--- src/main/java/com/matzip/api/domain/restaurant/service/RestaurantQueryServiceImpl.java
+++ src/main/java/com/matzip/api/domain/restaurant/service/RestaurantQueryServiceImpl.java
@@ -1,15 +1,20 @@
 package com.matzip.api.domain.restaurant.service;
 
 import com.matzip.api.common.error.RestaurantErrorCode;
+import com.matzip.api.common.event.DomainEventPublisher;
 import com.matzip.api.common.exception.ApiException;
 import com.matzip.api.domain.external.dto.RestaurantSearchResponse;
-import com.matzip.api.domain.recommendation.service.UserPreferenceService;
+import com.matzip.api.domain.external.service.RestaurantSearchService;
 import com.matzip.api.domain.restaurant.dto.RestaurantSearchCriteria;
 import com.matzip.api.domain.restaurant.dto.RestaurantSearchResult;
 import com.matzip.api.domain.restaurant.dto.RestaurantSearchResult.RestaurantDto;
 import com.matzip.api.domain.restaurant.entity.Restaurant;
+import com.matzip.api.domain.restaurant.event.RestaurantCreateRequestEvent;
 import com.matzip.api.domain.restaurant.repository.RestaurantRepository;
-import com.matzip.api.domain.review.service.ReviewService;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -23,13 +28,33 @@ public class RestaurantQueryServiceImpl implements
         RestaurantQueryService {
 
     private final RestaurantRepository restaurantRepository;
-    private final UserPreferenceService userPreferenceService;
-    private final ReviewService reviewService;
+    private final RestaurantSearchService restaurantSearchService;
     private final PersonalizedRatingService personalizedRatingService;
+    private final DomainEventPublisher eventPublisher;
 
     @Override
     public RestaurantSearchResult searchAndRecommendRestaurants(RestaurantSearchCriteria criteria) {
-        return null;
+        RestaurantSearchResponse externalResponse = getExternalRestaurant(criteria);
+        List<RestaurantSearchResult.RestaurantDto> restaurantDtos = new ArrayList<>();
+        for (RestaurantSearchResponse.Restaurant externalRestaurant : externalResponse.getRestaurants()) {
+            String externalId = externalRestaurant.generateId();
+            Optional<Restaurant> byExternalId = restaurantRepository.findByExternalId(externalId);
+            if (byExternalId.isEmpty()) {
+                eventPublisher.publish(new RestaurantCreateRequestEvent(externalRestaurant));
+                continue;
+            }
+            Restaurant internalRestaurant = byExternalId.get();
+
+            double personalizedScore = personalizedRatingService.calculateRating(criteria.getUserId(),
+                    internalRestaurant);
+            externalRestaurant.setRelevanceScore(personalizedScore);
+            RestaurantSearchResult.RestaurantDto dto = RestaurantSearchResult.RestaurantDto.from(externalRestaurant);
+            restaurantDtos.add(dto);
+        }
+        restaurantDtos.sort(
+                Comparator.comparingDouble(RestaurantSearchResult.RestaurantDto::getRelevanceScore).reversed());
+
+        return RestaurantSearchResult.from(restaurantDtos);
     }
 
     @Override
@@ -53,6 +78,13 @@ public class RestaurantQueryServiceImpl implements
                 .orElseThrow(() -> new ApiException(RestaurantErrorCode.RESTAURANT_NOT_FOUND));
 
         return personalizedRatingService.calculateRating(userId, restaurant);
+    }
 
+    private RestaurantSearchResponse getExternalRestaurant(RestaurantSearchCriteria criteria) {
+        return restaurantSearchService.searchRestaurants(
+                criteria.getLatitude(),
+                criteria.getLongitude(),
+                criteria.getQuery(),
+                criteria.getRadius());
     }
 }
